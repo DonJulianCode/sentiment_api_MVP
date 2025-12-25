da37ddd61aa27f73d68b9f55aea88745ed0005eb
Julián Gómez Brizuela <juliangomezb@gmail.com>
2025-12-25 13:59:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import joblib
+import numpy as np
 
 app = FastAPI(title="Sentiment API")
 
@@ -8,4 +9,12 @@ model = joblib.load("mvp_sentimientos.joblib")
 @app.post("/predict")
 def predict(text: str):
     pred = model.predict([text])[0]
-    return {"sentiment": pred}
+
+    # Probabilidades
+    probs = model.predict_proba([text])[0]
+    confidence = float(np.max(probs)) * 100
+
+    return {
+        "sentiment": pred,
+        "confidence": round(confidence, 2)
+    }
